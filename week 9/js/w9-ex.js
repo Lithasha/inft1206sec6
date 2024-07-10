1b607bcb80af3f73079844107552134148318d9c
Lithasha <lithasha.thanippuliappuhamilag@dcmail.ca>
2024-07-10 09:52:14 -0400

    week 9 example.html

---
+++ week 9/js/w9-ex.js
@@ -0,0 +1,10 @@
+console.log("Lithasha");
+let num = 5;
+let name = "Sam";
+console.log(num);
+
+// let firstName = window.prompt("what is your name?");
+let firstName = prompt("what is your name?");
+
+console.log(firstName);
+window.alert("Hello" + firstName);
