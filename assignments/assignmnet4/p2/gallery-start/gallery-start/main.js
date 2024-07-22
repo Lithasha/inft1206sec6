673da907ffd8da950bd50dd8168b15bdcd390c99
Lithasha <lithasha.thanippuliappuhamilag@dcmail.ca>
2024-07-22 13:30:12 -0400

    assignement4p2 - main.js - version 5
    added the darkened and lightened buttons. Also added the if else statements

--- assignments/assignmnet4/p2/gallery-start/gallery-start/main.js
+++ assignments/assignmnet4/p2/gallery-start/gallery-start/main.js
@@ -32,3 +32,16 @@ for(const image of pictures) {
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+    if (currentClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0,0,0,0)';
+    }
+
+});
