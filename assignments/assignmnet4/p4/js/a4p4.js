70ee5a5f830eeeef7ce5cc04881cb943bd5f71cc
Lithasha <lithasha.thanippuliappuhamilag@dcmail.ca>
2024-07-31 12:10:02 -0400

    a4p4.js - version 2
    added the comment header

--- assignments/assignmnet4/p4/js/a4p4.js
+++ assignments/assignmnet4/p4/js/a4p4.js
@@ -1,3 +1,11 @@
+/*
+Name: Lithasha Thanippuli Appuhamilage 
+Student No: 100942619
+Section: 6
+File: Assignment #4 P4
+Date: July 31th, 2024
+Description: This is the assignment #4 part 4; which is the JavaScript file of the sequencing annimations.
+ */
 const aliceTumbling = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
