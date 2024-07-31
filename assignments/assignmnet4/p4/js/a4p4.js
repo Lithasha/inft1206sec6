690dd61dbaaa91a6bc7ff36ac0d2c21cd521d03c
Lithasha <lithasha.thanippuliappuhamilag@dcmail.ca>
2024-07-31 12:41:48 -0400

    a4p4.js - version 4
    added the annimations to the other images

--- assignments/assignmnet4/p4/js/a4p4.js
+++ assignments/assignmnet4/p4/js/a4p4.js
@@ -21,4 +21,10 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
   
-  alice1.animate(aliceTumbling, aliceTiming);
+  alice1.animate(aliceTumbling, aliceTiming).finished.then(() => {
+    alice2.animate(aliceTumbling, aliceTiming).finished.then(() => {
+        alice3.animate(aliceTumbling, aliceTiming).finished.then(() => {
+            
+        });
+    });
+  });
