2a54808655d3d9851580e14b4a1275e5560a2a32
Lithasha <lithasha.thanippuliappuhamilag@dcmail.ca>
2024-07-31 12:22:51 -0400

    a4p4.js - version 3
    updated the JavaScript file to add a call to alice1.animate()

--- assignments/assignmnet4/p4/js/a4p4.js
+++ assignments/assignmnet4/p4/js/a4p4.js
@@ -7,16 +7,18 @@ Date: July 31th, 2024
 Description: This is the assignment #4 part 4; which is the JavaScript file of the sequencing annimations.
  */
 const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
   ];
   
   const aliceTiming = {
     duration: 2000,
     iterations: 1,
-    fill: 'forwards'
-  }
+    fill: "forwards",
+  };
   
   const alice1 = document.querySelector("#alice1");
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
+  
+  alice1.animate(aliceTumbling, aliceTiming);
