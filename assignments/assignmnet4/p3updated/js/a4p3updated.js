6237b4fb5cd9fcf457012ed4bd6d8dbd2b8d1c47
Lithasha <lithasha.thanippuliappuhamilag@dcmail.ca>
2024-08-02 19:10:11 -0400

    a4p3updated.js - version 4

--- assignments/assignmnet4/p3updated/js/a4p3updated.js
+++ assignments/assignmnet4/p3updated/js/a4p3updated.js
@@ -34,4 +34,19 @@ class Ball {
       this.color = color;
       this.size = size;
     }
-}  
+
+    collisionDetect() {
+    for (const ball of balls) {
+        if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+  
